45b8575f6341a0188918ec47b19f33bd6a2684b8
Cliff Chan <cchan17@live.ca>
2018-03-21 11:39:07 -0700

    added hero image and button

--- themes/b2w-theme/front-page.php
+++ themes/b2w-theme/front-page.php
@@ -10,6 +10,13 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<section class="front-page-hero">
+			<div class="hero-text-container">
+				<h1 class="hero-text">Be The Difference.</h1>
+				<button class="btn">Learn How</button>
+			</div>
+		</section>
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
