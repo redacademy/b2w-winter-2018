b8e0d04a4031bd39cdc881bd926410c03ce7160e
Cliff Chan <cchan17@live.ca>
2018-03-22 14:47:22 -0700

    style and functionality added to contact page

--- themes/b2w-theme/front-page.php
+++ themes/b2w-theme/front-page.php
@@ -26,28 +26,56 @@ get_header(); ?>
 				<div class="front-page-services">
 					<div class="front-page-services-text">	
 						<img src="<?php echo get_template_directory_uri() . '/images/paid-internship.svg' ?>">
-							<h2>Paid Internship</h2>
+							<h5>Paid Internship</h2>
 					</div>
 					<div class="front-page-services-text">
 							<img src="<?php echo get_template_directory_uri() . '/images/temp-placement.svg' ?>">
-							<h2>Temp Placement</h2>
+							<h5>Temp Placement</h2>
 					</div>
 					<div class="front-page-services-text">
 							<img src="<?php echo get_template_directory_uri() . '/images/permanant-placement.svg' ?>">
-							<h2>Temp to Placement</h2>
+							<h5>Temp to Placement</h2>
 					</div>
 				</div>
 		</section>
 
+		<section class="fp-container">
+			<div class="front-page-stats-box">
+				<div class="front-page-stats">
+					<h2>1 in 5</h2>
+					<p>children live in poverty and most them are from single parent homes.</p>
+				</div>
+				<div class="front-page-stats">
+					<h2>32,000</h2>
+					<p>single parents accessing income assistant in BC (May 2016)</p>
+				</div>
+			</div>
+			<div class="front-page-stats">
+				<h3>Most of them are single mothers.</h3>
+			</div>
+		</section>
+
 		<section>
-			<div>
-				<h2>1 in 5</h2>
-				<p>children live in poverty and most them are from single parent home.</p>
-			</div>
-			<div>
-				<h2>32,000</h2>
-				<p>children live in poverty and most them are from single parent home.</p>
-			</div>
+			<h2>What People Are Saying</h2>
+
+				<?php
+						$fields = CFS()->get( 'testimonial_loop' );
+						foreach ( $fields as $field ) {
+							$authorPhoto = $field['author_photo'];
+							?><img src="<?php echo $authorPhoto?>">
+							<?php
+						}
+					?>
+
+				<?php
+						$fields = CFS()->get( 'testimonial_loop' );
+						foreach ( $fields as $field ) {
+							$authorText = $field['testimonial_text'];
+							?><?php echo $authorText?>
+							<?php
+						}
+					?>
+
 		</section>
 
 
