8c1499b14f4a2652a4c7c8816e719f7b091847b6
Cliff Chan <cchan17@live.ca>
2018-03-23 16:15:06 -0700

    saved js file

--- themes/b2w-theme/js/b2w-scripts.js
+++ themes/b2w-theme/js/b2w-scripts.js
@@ -25,7 +25,6 @@
 
   /* Number Scroll */
 
-<<<<<<< HEAD
 
 //   function animateValue(id, start, end, duration) {
 //     var range = end - start;
@@ -54,23 +53,5 @@ function doSetTimeout(i) {
 
 for (var i = 1; i < 32001; i++)
   doSetTimeout(i);
-=======
-//   function animateValue(id, start, end, duration) {
-//     var range = end - start;
-//     var current = start;
-//     var increment = end > start? 1 : -1;
-//     var stepTime = Math.abs(Math.floor(duration / range));
-//     var obj = document.getElementById(id);
-//     var timer = setInterval(function() {
-//         current += increment;
-//         obj.innerHTML = current;
-//         if (current == end) {
-//             clearInterval(timer);
-//         }
-//     }, stepTime);
-// }
-
-// animateValue("value", 1, 32000, 1000);
->>>>>>> 836c2779b61db96bf09be25f5fe9ab39e3b903a7
 
 })(jQuery);
