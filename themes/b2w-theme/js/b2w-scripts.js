8578598cd3d2ca2b962ac20263381263a8efad5f
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-23 16:07:21 -0700

    Adding working functionality and style for faq accordion

--- themes/b2w-theme/js/b2w-scripts.js
+++ themes/b2w-theme/js/b2w-scripts.js
@@ -25,21 +25,21 @@
 
   /* Number Scroll */
 
-  function animateValue(id, start, end, duration) {
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-        current += increment;
-        obj.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
+//   function animateValue(id, start, end, duration) {
+//     var range = end - start;
+//     var current = start;
+//     var increment = end > start? 1 : -1;
+//     var stepTime = Math.abs(Math.floor(duration / range));
+//     var obj = document.getElementById(id);
+//     var timer = setInterval(function() {
+//         current += increment;
+//         obj.innerHTML = current;
+//         if (current == end) {
+//             clearInterval(timer);
+//         }
+//     }, stepTime);
+// }
 
-animateValue("value", 1, 32000, 1000);
+// animateValue("value", 1, 32000, 1000);
 
 })(jQuery);
