6aa48e1273fe6f1379acbb5bda7e85089940960c
Cliff Chan <cchan17@live.ca>
2018-03-26 13:15:36 -0700

    added desktop styling to services page

--- themes/b2w-theme/page-services.php
+++ themes/b2w-theme/page-services.php
@@ -14,33 +14,39 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				
-					<?php echo CFS()->get( 'services_internship' );?>
-					<?php echo CFS()->get( 'services_placement' );?>
-					<?php echo CFS()->get( 'services_permanent' );?>
-					<img src="<?php echo CFS()->get( 'services_image' );?>">
+					<div class="services-container-desktop">
+					<div>
+						<h1>What We Do at B2W</h1>
+							<div class="services-hide"><?php echo CFS()->get( 'services_info' );?></div>
+					</div>
+						<div class="services-hide-desktop">
+							<?php echo CFS()->get( 'services_internship' );?>
+							<?php echo CFS()->get( 'services_placement' );?>
+							<?php echo CFS()->get( 'services_permanent' );?>
+						</div>
+						<img src="<?php echo CFS()->get( 'services_image' );?>">
+					</div>
 
 					<h1>Why Hire Temp Staff?</h1>
-					<?php echo CFS()->get( 'services_hire_temp' );?>
-					<img src="<?php echo CFS()->get( 'services_image2' );?>">
+						<?php echo CFS()->get( 'services_hire_temp' );?>
+						<img src="<?php echo CFS()->get( 'services_image2' );?>">
 
 					<h1>What People Are Saying</h1>
-
-				<?php
-						$fields = CFS()->get( 'testimonial_loop' );
-						foreach ( $fields as $field ) {
-							$authorPhoto = $field['author_photo'];
-							$authorName = $field['author_name'];
-							$authorText = $field['testimonial_text'];
+						<?php
+								$fields = CFS()->get( 'testimonial_loop' );
+								foreach ( $fields as $field ) {
+									$authorPhoto = $field['author_photo'];
+									$authorName = $field['author_name'];
+									$authorText = $field['testimonial_text'];
+									?>
+									<div class="service-page-testimonials">
+										<img src="<?php echo $authorPhoto?>">
+										<?php echo $authorName?>
+										<?php echo $authorText?>
+									</div>
+									<?php
+								}
 							?>
-							<div class="service-page-testimonials">
-								<img src="<?php echo $authorPhoto?>">
-								<?php echo $authorName?>
-								<?php echo $authorText?>
-							</div>
-							<?php
-						}
-					?>
 
 			<?php endwhile; // End of the loop. ?>
 
