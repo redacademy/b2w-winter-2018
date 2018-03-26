b263bfb6d43f9b72e916c1fb1357fbc3f16f804d
Eric Chan <ericchan277@gmail.com>
2018-03-25 20:11:52 -0700

    Merge conflict

--- themes/b2w-theme/js/b2w-scripts.js
+++ themes/b2w-theme/js/b2w-scripts.js
@@ -21,37 +21,52 @@
     /* Mobile Menu Header*/
 
     $('.menu').prepend('<p class="menu-header">' + 'Menu' + '</p>');
+
+    /* Number Scroll */
+
+
+    //   function animateValue(id, start, end, duration) {
+    //     var range = end - start;
+    //     var current = start;
+    //     // var increment = end > start? 1 : -1;
+    //     var stepTime = Math.abs(Math.floor(duration / range));
+    //     var obj = document.getElementById(id);
+    //     var timer = setInterval(function() {
+    //         // current += increment;
+    //         current += 100;
+    //         obj.innerHTML = current;
+    //         if (current >= end) {
+    //             clearInterval(timer);
+
+    //             obj.innerHTML = end;
+    //         }
+    //     }, stepTime);
+    // }
+    // animateValue("value", 1, 32000, 100);
+
+
+    /* Custom Number Animation JS */
+
+      // function numberCounter(num) {
+      //   setTimeout(function () {
+      //     $('#value').text(num)
+      //   }, 100);
+      // }
+
+      // var countYval = 300;
+
+      // $(window).scroll(function () {
+      //   var offset = $(window).scrollTop();
+      //   if (offset > countYval) {
+      //     countYval += 300;
+      //     for (var num = 1; num < 32001; num++) {
+      //       numberCounter(num);
+      //     }
+      //   }
+      // });
+
+      var tester = $("#value").val().length;
+      console.log (tester);
+
   });
-
-  /* Number Scroll */
-
-
-//   function animateValue(id, start, end, duration) {
-//     var range = end - start;
-//     var current = start;
-//     // var increment = end > start? 1 : -1;
-//     var stepTime = Math.abs(Math.floor(duration / range));
-//     var obj = document.getElementById(id);
-//     var timer = setInterval(function() {
-//         // current += increment;
-//         current += 100;
-//         obj.innerHTML = current;
-//         if (current >= end) {
-//             clearInterval(timer);
-
-//             obj.innerHTML = end;
-//         }
-//     }, stepTime);
-// }
-// animateValue("value", 1, 32000, 100);
-
-function numberCounter(num) {
-  setTimeout(function () {
-    $('#value').text(num)
-  }, 100);
-}
-
-for (var num = 1; num < 32001; num++){
-  numberCounter(num);
-}
 })(jQuery);
