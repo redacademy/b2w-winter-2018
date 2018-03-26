8f33be5371a1567487b1c5db2ef1d66336b2f001
Eric Chan <ericchan277@gmail.com>
2018-03-26 00:03:52 -0700

    Added footer css

--- themes/b2w-theme/footer.php
+++ themes/b2w-theme/footer.php
@@ -12,15 +12,17 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
 				</div><!-- .site-info -->
-
-				<div class="footer-logo">
-					<img src="<?php echo get_template_directory_uri() . '/images/B2W_logo_final.png'?>">
-				</div>
 				
-				<div class="widget-container">
-					<?php if ( is_active_sidebar( 'footer-sidebar') ) : ?>
-						<?php dynamic_sidebar( 'footer-sidebar' ); ?>
-					<?php endif; ?>
+				<div class="footer-logo-widget-wrapper">
+					<div class="footer-logo">
+						<img src="<?php echo get_template_directory_uri() . '/images/B2W_logo_final.png'?>">
+					</div>
+				
+					<div class="widget-container">
+						<?php if ( is_active_sidebar( 'footer-sidebar') ) : ?>
+							<?php dynamic_sidebar( 'footer-sidebar' ); ?>
+						<?php endif; ?>
+					</div>
 				</div>
 
 				<p class="footer-copy"> &copy; 2018 Bridge2Work - Thriving Generation Employment Agency CCC Inc. </p> 
