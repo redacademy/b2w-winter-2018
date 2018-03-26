e6150068f7c0c7f767e07c12745101856f0353dd
Rodrigo Samayoa <jorosafi@gmail.com>
2018-03-26 15:55:46 -0700

    Adding pop up thank you for contact form

--- themes/b2w-theme/js/faq.js
+++ themes/b2w-theme/js/faq.js
@@ -27,5 +27,15 @@
 
     $('.faq-minus').on('click', closeAccordionSection);
 
+  //Contact Form Thank You Pop Up
+  $('.wpcf7-submit').on('click', function(){
+    setTimeout(function(){
+      if($('.wpcf7-mail-sent-ok').is(':visible')){
+        $('.wpcf7-mail-sent-ok').empty().append('<div class="thank-you-container"><p>We appreciate your time. We will get back to you as soon as possible.</p><a href="" id="thank-you-close-btn" class="btn-plain thank-you-btn">Okay</a></div>');
+      }
+    }, 500);
+
+    
+  });
   });
 })(jQuery);
