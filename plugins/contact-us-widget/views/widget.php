7709bc1b5124558e3b20a3c4a7374bc8821d24e7
Eric Chan <ericchan277@gmail.com>
2018-03-25 21:54:55 -0700

    Refactored contact widget classes for easier css manipulation

--- plugins/contact-us-widget/views/widget.php
+++ plugins/contact-us-widget/views/widget.php
@@ -1,22 +1,24 @@
 <!-- This file is used to markup the public-facing widget. -->
 
 <?php if(strlen( trim( $email ) )>0): ?>
-<p>
-  <span class="email-span"></span>
+
+<p class="email-text">
   <?php echo $email; ?>
 </p>
+
 <?php endif; ?>
 
 <?php if(strlen( trim( $location ) )>0): ?>
-<p>
-  <span class="location-span"></span>
-  <?php echo $location; ?>
+ 
+<p class="location-text">
+   <?php echo $location; ?>
 </p>
+
 <?php endif; ?>
 
 <?php if(strlen( trim( $phone_number ) )>0): ?>
-<p>
-  <span class="phone-number-span"></span>
+
+<p class= "phone-num-text">
   <?php echo $phone_number; ?>
 </p>
 <?php endif; ?>
