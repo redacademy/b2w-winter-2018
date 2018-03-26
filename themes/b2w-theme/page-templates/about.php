3e68310916fdcc0bf057683c72b3db07a86f1ac5
Eric Chan <ericchan277@gmail.com>
2018-03-26 00:18:24 -0700

    Added comments to footer and about page css

--- themes/b2w-theme/page-templates/about.php
+++ themes/b2w-theme/page-templates/about.php
@@ -7,29 +7,30 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-
-		<h2> <span class="underline">What</span> <span class="title-norm">We're All About</span></h2>
-			<div class="about-text">
-				<?php echo CFS()->get( 'about_blurb' );?>
-			</div>
+			<section class="what-about-wrapper">
+				<h2> <span class="underline">What</span> <span class="title-norm">We're All About</span></h2>
+					<div class="about-text">
+						<?php echo CFS()->get( 'about_blurb' );?>
+					</div>
+			</section>
 		
-		<section class="who-we-are-wrapper">
-			<h2> <span class="underline">Who</span> <span class="title-norm">We Are</span></h2>
-			<div class="employee-wrapper">
-				<?php
-					$fields = CFS()->get( 'employee' );
-						foreach ( $fields as $field ) {
-							?> <div class="person-container">
-							<div class="emp-photo">
-							<img src="<?php echo $field['employee_picture'] ?>">
-						</div>
-					<p class="emp-name"><?php	echo $field['employee_name']?></p> 
-						</div>
+			<section class="who-we-are-wrapper">
+				<h2> <span class="underline">Who</span> <span class="title-norm">We Are</span></h2>
+				<div class="employee-wrapper">
+					<?php
+						$fields = CFS()->get( 'employee' );
+							foreach ( $fields as $field ) {
+					?>	<div class="person-container">
+								<div class="emp-photo">
+									<img src="<?php echo $field['employee_picture'] ?>">
+								</div>
+								<p class="emp-name"><?php	echo $field['employee_name']?></p> 
+							</div>
 						<?php
-						}	
+							}	
 					?>
-			</div>
-		</section>
+				</div>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
